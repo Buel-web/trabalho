3be9ff35bf4081a625f496ba95d76c66e7e4f62d
Samuel De Souza <samuelsouza2220@gmail.com>
2026-09-05 19:49:24 -0300

    Bug resolvido

--- src/main/java/com/MinhaAPi/demo/entities/Categoria.java
+++ src/main/java/com/MinhaAPi/demo/entities/Categoria.java
@@ -1,4 +1,15 @@
 package com.MinhaAPi.demo.entities;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "categorias")
 public class Categoria {
+
+    @Id
+    private String id;
+
+    private String nome;
 }
